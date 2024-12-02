f7fc873fc1ecd4e9508a520ae71c9002c3d2097c
mikayilov2006@gmail.com <mikayilov2006@gmail.com>
2024-12-02 10:13:35 +0400

    privateRoute

---
+++ client/src/privateRoute/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const PrivateRoute = ({children}:any) => {
+  return children
+}
+
+export default PrivateRoute
--- client/src/routers/index.tsx
+++ client/src/routers/index.tsx
@@ -1,10 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
-import Home from "../components/home";
+import Home from "../pages/home";
+import PrivateRoute from "../privateRoute";
 
 export const routers  = createBrowserRouter([
     {
         index:true,
-        element:<Home/>
+        element:<PrivateRoute>
+            <Home/>
+        </PrivateRoute>
     },
     
 ])
